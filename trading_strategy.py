aee7bc64b06dde9ea7d366a383cd09318e6f56cc
Craig Belshe <craig.belshe@bitstamp.net>
2019-07-19 14:07:45 +0200

    bug fix

--- trading_strategy.py
+++ trading_strategy.py
@@ -5,14 +5,6 @@ class TradingStrategy:
         self.pair = pair
 
     def calc_mv_avg(self, period):
-        md = MarketDataInterface(self.pair)
-        data = md.get_all_ticker(period)
-        sums = 0
-        div = 1
-        for ticker in data:
-            sums += ticker['value']
-            div += 1
-        return sums/div
 
     def get_actions(self, prev_ticker, current_ticker):
         return actions
