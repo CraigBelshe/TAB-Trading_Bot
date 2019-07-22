e04b8f0de3f8dcd04a3bbec66d401e730b719c02
Craig Belshe <craig.belshe@bitstamp.net>
2019-07-22 11:15:22 +0200

    rm unused code

--- trading_strategy.py
+++ trading_strategy.py
@@ -1,15 +1,17 @@
 from market_data import MarketDataInterface
 
+
 class TradingStrategy:
     def __init__(self, pair):
-        self.pair = pair
+        pass
 
     def calc_mv_avg(self, period):
+        pass
 
     def get_actions(self, prev_ticker, current_ticker):
-        return actions
+        pass
 
     def get_risk(self, prev_ticker, current_ticker):
-        return percent_amount
+        pass
 
 
