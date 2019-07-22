b7841977bd8fe186969b76d8ee8a46762c0fc924
Craig Belshe <craig.belshe@bitstamp.net>
2019-07-22 10:22:11 +0200

    initial unittests

---
+++ unittest.py
@@ -0,0 +1,17 @@
+import unittest
+import mock
+
+
+class UnitTest(unittest.UnitTest):
+    def __init__(self):
+        pass
+
+    def test_get_signature(self):
+
+    def test_get_balance(self):
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
